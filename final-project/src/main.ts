4e9ef198f9a9053976b594a8436f5fa9381212f8
Jakub <gofdark231@gmail.com>
2024-06-11 23:17:03 +0200

    updated cors

--- final-project/src/main.ts
+++ final-project/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: process.env.CLIENT,
+  });
 
   await app.listen(3000);
 }
